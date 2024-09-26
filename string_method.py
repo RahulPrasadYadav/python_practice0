1b16fc8021a24a835384d0f76285c56c6042deed
Rahul Prasad Yadav <90641918+RahulPrasadYadav@users.noreply.github.com>
2024-09-26 13:31:57 +0000

    string method

---
+++ string_method.py
@@ -0,0 +1,35 @@
+a="Rahul"
+print(len(a)) # length 5
+
+print(a.upper())   # output will be RAHUL 
+
+print(a.lower())  #rahul
+
+# rstrip method
+str="Rahul !!! !!!!!!  !!!!!!!! !!!!!"
+print(str.rstrip("!"))  
+
+# replace method 
+
+print(a.replace("Rahul","Yadav"))
+
+# split() 
+
+print(str.split(" "))
+
+
+blogHeading="welcome to python class "
+print(blogHeading.capitalize())
+
+str1="welcome to console"
+print(len(str1))
+print(str1.center(50))
+
+#count()
+
+str2="Rahuuuul"
+print(str2.count("u"))  # 4 output u is  4 time
+
+str3="welcome to my world "
+print(str3.find("to"))
+print(str3.index("to"))
