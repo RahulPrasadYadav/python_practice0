6658118169381bc1f548d0d88f281518206ba9cd
Rahul Prasad Yadav <90641918+RahulPrasadYadav@users.noreply.github.com>
2024-09-26 07:28:38 +0000

    string added

---
+++ string.py
@@ -0,0 +1,27 @@
+text="ice cream"
+print(text)
+print(text[0])
+
+print(text[0:3])
+
+print(text[4:9])
+
+adress=''' kolkata west bemgal 
+   dist=hooghly 
+   pin=712123
+
+ '''
+
+print(adress)
+
+
+s1="hello"
+s2="world"
+print(s1+s2)
+
+
+s='total state in use'
+num=25
+
+
+print(s+ str(num))
