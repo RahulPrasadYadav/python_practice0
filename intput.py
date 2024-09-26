aeb50910ccfc7094d4b4af4b410422275ccbf91d
Rahul Prasad Yadav <90641918+RahulPrasadYadav@users.noreply.github.com>
2024-09-26 12:52:37 +0000

    added input

---
+++ intput.py
@@ -0,0 +1,14 @@
+# a=input("enter thr name: ")
+
+# print ("my name is : ",a)
+
+
+x=input("enter the number: ")
+y=input("enter the number: ")
+
+print(x+y)  # simply teking 1 , 1 input . this can be print 11 ..why bcoz this is string concat
+
+
+# print int 
+
+print(int(x)+int(y))
