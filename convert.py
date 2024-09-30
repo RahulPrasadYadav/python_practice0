d6c7ec5216b0cc8f7b0b3ec1f477af18e7830a4e
Rahul Prasad Yadav <90641918+RahulPrasadYadav@users.noreply.github.com>
2024-09-30 04:10:53 +0000

    added convert decimal to hexadecimal

---
+++ convert.py
@@ -0,0 +1,27 @@
+
+print(int("12",8))
+print(int("123",8))
+print(int("34",8))
+
+#convert to decimal to hexadecimal 
+
+print(int("19",16))
+print(int("4f",16))
+print(int("98",16))
+
+
+#bool(value)
+
+print(bool([]))
+print(bool(["a value"]))
+print(bool([None]))
+print(bool([0]))
+print(bool([1]))
+
+
+# typecasting
+a=1
+print(type(a))
+print(a)
+b=float(a)
+print(type(b))
