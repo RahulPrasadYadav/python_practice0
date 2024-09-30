17110eca1cfa8a412c111f2985f9c1adc53d3bc7
Rahul Prasad Yadav <90641918+RahulPrasadYadav@users.noreply.github.com>
2024-09-30 12:31:40 +0000

    loops added

---
+++ loop.py
@@ -0,0 +1,23 @@
+# name ='Rahul'
+# for i in name:
+#     print(i)
+#     if(i=="u"):
+#         print("this is something special")
+
+
+
+colors=["red","green","black","yellow"]
+
+for color in colors:
+    print(color)
+# for i in color:
+#     print(i)
+
+for i in range(10):
+    print(i)
+
+for i in range(10):
+    print(i+1)
+
+for i in range(1,10):
+    print(i)
