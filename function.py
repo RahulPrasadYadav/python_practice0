8f371eb870500b652e9188b6fcf0f84e2484fc08
Rahul Prasad Yadav <90641918+RahulPrasadYadav@users.noreply.github.com>
2024-10-01 07:28:37 +0000

    added function

---
+++ function.py
@@ -0,0 +1,22 @@
+def calmean(a,b):
+    mean=(a*b)/(a+b)
+    print(mean)
+def isGreater(a,b):
+    if(a>b):
+        print("first is greater")
+    else:
+        print("second is greater")
+
+def islesser(a,b):
+    pass
+
+
+a=8
+b=9
+calmean(a,b)
+isGreater(a,b)
+
+c=9
+d=10
+calmean(c,d)
+isGreater(c,d)
