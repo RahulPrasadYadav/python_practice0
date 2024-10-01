44ad48baebea79228473bd9ca63353f8cee80690
Rahul Prasad Yadav <90641918+RahulPrasadYadav@users.noreply.github.com>
2024-10-01 04:39:23 +0000

    added typcasting and loop

---
+++ while_loop.py
@@ -0,0 +1,13 @@
+# i=0
+# while(i<=3):
+#     print(i)
+#     i=i+1
+
+# print("done with loop")
+
+
+i=int(input("Enter the number"))
+while(i<38):
+    i=int(input("enter the number"))
+    print(i)
+    i=i+1
