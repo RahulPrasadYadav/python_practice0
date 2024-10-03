c58d8fa03d7d07f354a54540ac8b48d8832c3873
Rahul Prasad Yadav <90641918+RahulPrasadYadav@users.noreply.github.com>
2024-10-03 04:12:44 +0000

    added or , and , ex or

--- date03_10.py
+++ date03_10.py
@@ -5,9 +5,22 @@ print(a&b)
 # and oprater
 a=50; b=90
 print(a&b)
-c=111
-e=222
+c=44
+e=144
 print(bin(c))
 print(bin(e))
-print(c&e)
+print(c^e)
 
+x=71
+y=91
+print(x&y)
+
+
+x=71
+y=91
+print(x^y)
+
+
+x=71
+y=91
+print(x^y)
