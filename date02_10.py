3d4b25a88d200ef0021762a4e97d6859b8349978
Rahul Prasad Yadav <90641918+RahulPrasadYadav@users.noreply.github.com>
2024-10-03 04:05:49 +0000

    added and op in python

---
+++ date02_10.py
@@ -0,0 +1,8 @@
+a=b=[1,2,3]
+c=[1,2,3]
+print(a is b)
+print(a is c)
+print(id(a))
+print(id(b))
+print(id(c))
+
---
+++ date03_10.py
@@ -0,0 +1,13 @@
+a=10;b=20
+print(a&b)
+
+
+# and oprater
+a=50; b=90
+print(a&b)
+c=111
+e=222
+print(bin(c))
+print(bin(e))
+print(c&e)
+
