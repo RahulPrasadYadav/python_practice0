6114617984bd8cc6f31272591d36abe82bf40fa5
Rahul Prasad Yadav <90641918+RahulPrasadYadav@users.noreply.github.com>
2024-10-09 04:03:12 +0000

    added loop

---
+++ list2.py
@@ -0,0 +1,9 @@
+l=[1,2,3,4,"Rahul"]
+print(l)
+print(type(l))
+
+if "6"in l:
+    print("Present")
+else:
+    print("NO")
+
---
+++ loop2.py
@@ -0,0 +1,15 @@
+
+
+number = [1,23,4,5,6,7,8,9]
+sum=0
+
+for val in number:
+    sum=sum+val
+print(val)
+print("total sum  is",sum)
+
+
+data={12,4,5,36,2}
+for x in data:
+    print(x,end=" ")
+    
