fcac33b02569ae8dad4e695c0ebed176ebb6bf9e
Rahul Prasad Yadav <90641918+RahulPrasadYadav@users.noreply.github.com>
2024-10-21 03:52:36 +0000

    tuple add

---
+++ tuple.py
@@ -0,0 +1,13 @@
+a=1,2,3,4   #tuple packing
+print(type(a))
+
+b=1,
+print(type(b))
+
+
+pytuple=(2,4,3,5,4,6,7,8,6,1)
+print(pytuple[3:5])
+print(pytuple[:6])
+print(pytuple[5:])
+print(pytuple[:])
+print(pytuple[-8:])
